84f68cefeeb64a637853c67a7a00f9b2cbe49fe3
emna mekni <myboostapp0@gmail.com>
2026-03-31 10:21:24 +0100

    optimize model loading

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from transformers import pipeline
 import streamlit as st
 
-# Load model
-#chatbot = pipeline("text-generation", model="gpt2")
-chatbot = pipeline("text-generation", model="distilgpt2")
+
+@st.cache_resource
+def load_model():
+    return pipeline("text-generation", model="distilgpt2")
+
+chatbot = load_model()
 
 st.title("AI Chatbot 🤖")
 
@@ -12,3 +15,16 @@ user_input = st.text_input("Ask something:")
 if user_input:
     response = chatbot(user_input, max_length=100)
     st.write(response[0]['generated_text'])
+
+
+# Load model
+#chatbot = pipeline("text-generation"""  """", model="gpt2")
+# chatbot = pipeline("text-generation", model="distilgpt2")
+
+# st.title("AI Chatbot 🤖")
+
+# user_input = st.text_input("Ask something:")
+
+# if user_input:
+#     response = chatbot(user_input, max_length=100)
+#     st.write(response[0]['generated_text'])
