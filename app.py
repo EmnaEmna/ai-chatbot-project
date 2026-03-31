45c90a04aedc34eef8e35eb4de7fd3a5c353f046
emna mekni <myboostapp0@gmail.com>
2026-03-31 09:47:20 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from transformers import pipeline
+import streamlit as st
+
+# Load model
+chatbot = pipeline("text-generation", model="gpt2")
+
+st.title("AI Chatbot 🤖")
+
+user_input = st.text_input("Ask something:")
+
+if user_input:
+    response = chatbot(user_input, max_length=100)
+    st.write(response[0]['generated_text'])
