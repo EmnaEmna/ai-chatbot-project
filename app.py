1993e86e93746828661d7bdc276788cad2f232fa
emna mekni <myboostapp0@gmail.com>
2026-03-31 10:48:59 +0100

    response-improvement

--- app.py
+++ app.py
@@ -13,7 +13,16 @@ st.title("AI Chatbot 🤖")
 user_input = st.text_input("Ask something:")
 
 if user_input:
-    response = chatbot(user_input, max_length=100)
+    #response = chatbot(user_input, max_length=100)
+    response = chatbot(
+    user_input,
+    max_length=100,
+    do_sample=True,
+    temperature=0.7,
+    top_k=50,
+    top_p=0.9,
+    repetition_penalty=1.2
+)
     st.write(response[0]['generated_text'])
 
 
